404a43243bfbabb1e4bf1df5809b37ed0d01c768
Jayson Lutrario <jaysonlutrario@gmail.com>
2020-09-22 14:14:22 -0400

    Started unit testing for UserService

---
+++ FitBuddy/src/test/java/com/revature/services/UserServiceTest.java
@@ -0,0 +1,40 @@
+package com.revature.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.revature.models.User;
+import com.revature.repositories.IUserDAO;
+
+public class UserServiceTest {
+
+	@InjectMocks
+	private UserService userServ;
+	
+	@Mock
+	private IUserDAO userDao;
+		
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testRegisterSuccess() {
+		User test = new User(0, "test", "test", "Test", "Test", "test@mail.com", 2000);
+		when(userDao.insert(test)).thenReturn(1);
+		when(userDao.update(test)).thenReturn(new User(1, "test", "test", "Test", "Test", "test@mail.com", 2000));
+		
+		test.setId(1);
+		User registered = userServ.register(test);
+		
+		assertEquals(registered, test);
+	}
+
+}
