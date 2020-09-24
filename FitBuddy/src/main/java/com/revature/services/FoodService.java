c2104d8cc6e4da9f54029d310980858d54f49fde
Jayson Lutrario <jaysonlutrario@gmail.com>
2020-09-23 22:33:16 -0400

    Implemented meal & food services

---
+++ FitBuddy/src/main/java/com/revature/services/FoodService.java
@@ -0,0 +1,71 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.models.Food;
+import com.revature.models.Meal;
+import com.revature.repositories.IFoodDAO;
+import com.revature.repositories.IMealDAO;
+
+@Service
+public class FoodService {
+
+	@Autowired
+	private IFoodDAO foodDao;
+	
+	@Autowired
+	private IMealDAO mealDao;
+	
+	public List<Food> getAllFood() {
+		return foodDao.allFood();
+	}
+	
+	public List<Food> foodByMeal(Meal m) {
+		return foodDao.findByMeal(m);
+	}
+	
+	public List<Food> foodByMeal(int mealId) {
+		Meal m = mealDao.findById(mealId);
+		
+		if (m != null) {
+			return foodDao.findByMeal(m);
+		}
+		return null;
+	}
+	
+	public Food addFood(Food f, Meal m) {
+		f.setMeal(m);
+		return update(f);
+	}
+	
+	public Food addFood(Food f, int mealId) {
+		Meal m = mealDao.findById(mealId);
+		
+		if (m != null) {
+			f.setMeal(m);
+			return update(f);
+		}
+		return null;
+	}
+	
+	public Food insert(Food f) {
+		int id = foodDao.insert(f);
+		
+		if (id != 0) {
+			f.setId(id);
+			return update(f);
+		}
+		return null;
+	}
+	
+	public Food update(Food f) {
+		return foodDao.update(f);
+	}
+	
+	public void delete(Food f) {
+		foodDao.delete(f);
+	}
+}
---
+++ FitBuddy/src/main/java/com/revature/services/MealService.java
@@ -0,0 +1,85 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.models.Meal;
+import com.revature.models.User;
+import com.revature.repositories.IMealDAO;
+import com.revature.repositories.IUserDAO;
+
+@Service
+public class MealService {
+
+	@Autowired
+	private IMealDAO mealDao;
+	
+	@Autowired
+	private IUserDAO userDao;
+	
+	public List<Meal> getAllMeals() {
+		return mealDao.allMeals();
+	}
+	
+	public List<Meal> mealsByUser(User u) {
+		return mealDao.findByUser(u);
+	}
+	
+	public List<Meal> mealsByUser(int userId) {
+		User u = userDao.findById(userId);
+		
+		if (u != null) {
+			return mealDao.findByUser(u);
+		}
+		return null;
+	}
+	
+	public Meal findMeal(int id) {
+		return mealDao.findById(id);
+	}
+	
+	public Meal newMeal(Meal m) {
+		int id = mealDao.insert(m);
+		
+		if (id != 0) {
+			m.setId(id);
+			return mealDao.update(m);
+		}
+		return null;
+	}
+	
+	public Meal newMeal(Meal m, User u) {
+		m.setUser(u);
+		return update(m);
+	}
+	
+	public Meal newMeal(Meal m, int userId) {
+		User u = userDao.findById(userId);
+		
+		if (u != null) {
+			m.setUser(u);
+			return update(m);
+		}
+		return null;
+	}
+	
+	public Meal insert(Meal m) {
+		int id = mealDao.insert(m);
+		
+		if (id != 0) {
+			m.setId(id);
+			return update(m);
+		}
+		return null;
+	}
+	
+	public Meal update(Meal m) {
+		return mealDao.update(m);
+	}
+	
+	public void delete(Meal m) {
+		mealDao.delete(m);
+	}
+}
