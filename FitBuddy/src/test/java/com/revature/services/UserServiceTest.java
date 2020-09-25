b7c26b23df36590670aa9e170be63df1a0e1109c
Jayson Lutrario <jaysonlutrario@gmail.com>
2020-09-24 23:46:35 -0400

    Added tests & jacoco

---
+++ FitBuddy/src/test/java/com/revature/services/UserServiceTest.java
@@ -0,0 +1,77 @@
+package com.revature.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.revature.models.User;
+import com.revature.repositories.IUserDAO;
+
+public class UserServiceTest {
+
+	@InjectMocks
+	private UserService userServ;
+	
+	@Mock
+	private IUserDAO userDao;
+		
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testRegisterSuccess() {
+		User test = new User(0, "test", "test", "Test", "Test", "test@mail.com", 2000);
+		when(userDao.insert(test)).thenReturn(1);
+		when(userDao.update(test)).thenReturn(new User(1, "test", BCrypt.hashpw("test", BCrypt.gensalt()), "Test", "Test", "test@mail.com", 2000));
+		
+		User registered = userServ.register(test);
+		
+		assertEquals(registered.getId(), 1);
+		assertNotEquals(registered.getPassword(), "test");
+	}
+
+	@Test
+	public void testUsernameTakenFailure() {
+		User test = new User(0, "test", "test", "Test", "Test", "test@mail.com", 2000);
+		when(userDao.insert(test)).thenReturn(0);
+		when(userDao.update(test)).thenReturn(new User(1, "test", BCrypt.hashpw("test", BCrypt.gensalt()), "Test", "Test", "test@mail.com", 2000));
+		
+		User registered = userServ.register(test);
+		
+		assertEquals(registered, null);
+	}
+	
+	@Test
+	public void testLoginSuccess() {
+		User test = new User(0, "test", BCrypt.hashpw("test", BCrypt.gensalt()), "Test", "Test", "test@mail.com", 2000);
+		when(userDao.findByUsername("test")).thenReturn(test);
+		
+		assertEquals(userServ.login("test", "test"), test);
+	}
+	
+	@Test
+	public void testIncorrectPasswordFailure() {
+		User test = new User(0, "test", BCrypt.hashpw("test", BCrypt.gensalt()), "Test", "Test", "test@mail.com", 2000);
+		when(userDao.findByUsername("test")).thenReturn(test);
+		
+		assertEquals(userServ.login("test", "wrongPW"), null);
+	}
+	
+	@Test
+	public void testUsernameNotFoundFailure() {
+		User test = new User(0, "test", BCrypt.hashpw("test", BCrypt.gensalt()), "Test", "Test", "test@mail.com", 2000);
+		when(userDao.findByUsername("test")).thenReturn(test);
+		
+		assertEquals(userServ.login("wrongUN", "test"), null);
+	}
+	
+}
