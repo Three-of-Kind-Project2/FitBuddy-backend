d03fcdb1d656dc80d57a228265938e3dcf40b350
mtaew <mtae122@gmail.com>
2020-09-26 00:00:36 -0500

    Meal can be added

--- FitBuddy/src/main/java/com/revature/controllers/MealController.java
+++ FitBuddy/src/main/java/com/revature/controllers/MealController.java
@@ -1,56 +1,46 @@
 package com.revature.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.models.Food;
-import com.revature.models.FoodDTO;
 import com.revature.models.Meal;
 import com.revature.models.MealDTO;
-import com.revature.services.FoodService;
-import com.revature.services.MealService;
-
-@RequestMapping("log")
+import com.revature.services.MealService
+;
+import com.revature.services.UserService;
+@RequestMapping("meal")
 @CrossOrigin("http://localhost:4200")
 @RestController
-public class LogController {
-
+public class MealController {
+	
 	@Autowired
 	MealService mealServ;
 	
 	@Autowired
-	FoodService foodServ;
+	UserService userServ;
 	
-
-	@GetMapping
-	public ResponseEntity<List<Meal>> getMeals(int user_id) {
-		return ResponseEntity.ok(this.mealServ.mealsByUser(user_id));
-	}
-	
-
 	@PostMapping
-	public ResponseEntity<List<Meal>> addFood(@RequestBody FoodDTO f) {
-//		Meal meal = mealServ.findMeal(f.getMeal_id());
-		Meal meal = null;
-		Food food = new Food(f.getFood_id(), f.getName(), f.getUrl(), f.getCalories(), f.getCarbs(), 
-				f.getProtein(), f.getFat(), meal);
+	public ResponseEntity<List<Meal>> addMeal(@RequestBody MealDTO m) {
 		
-		if (food.getId() != 0) {
+		Meal meal = new Meal(m.getId(), LocalDateTime.now(), m.getType(), userServ.findUser(m.getUserid()));
+		
+		if (meal.getId() != 0) {
 			return ResponseEntity.badRequest().build();
 		}
 		
-		this.foodServ.insert(food);
+		this.mealServ.insert(meal);
 		
-		if (food.getId() == 0) {
+		if (meal.getId() == 0) {
 			// could not save properly
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
---
+++ FitBuddy/src/main/java/com/revature/models/MealDTO.java
@@ -0,0 +1,34 @@
+package com.revature.models;
+
+
+public class MealDTO {
+	private int id;
+	private String time;
+	private String type;
+	private int userid;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+}
