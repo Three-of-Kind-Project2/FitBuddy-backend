de65b848c8c86713badc93855f83f2daedea87d4
pranavshp <udayakumarp2@vcu.edu>
2020-09-26 14:42:37 -0400

    Added completed selenium log in test

--- FitBuddy/src/test/java/com/revature/pages/FitBuddyLogin.java
+++ FitBuddy/src/test/java/com/revature/pages/FitBuddyLogin.java
@@ -6,26 +6,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class FitBuddyLogin {
+
+	public static final String title = "FitBuddy"; 
 	
-	public final String title = "Angular"; 
+	// after logging in succesfully find an h3 on the page saying consumed table
+	@FindBy(xpath = "//h3[text()='Consumed Table']")
+	public WebElement header; 
 	
-	@FindBy(xpath = "//input[@name='username']")
-	//maps to username on page
-	public WebElement username; 
-	//maps to password on page
-    @FindBy(name = "password")
-	public WebElement password; 
-    //maps to submit button on page
-    @FindBy(xpath = "//input[@type='submit']")
-    public WebElement submitButton; 
-    //Fitbuddy Login webdriver method in constructor
-    public FitBuddyLogin (WebDriver driver) {
-    	PageFactory.initElements(driver, this);
-    }
-   // method for logging in on webpage
-    public void loginToFitBuddyLogin(String username, String password) {
-    	this.username.sendKeys(username);
-    	this.password.sendKeys(password);
-    	this.submitButton.click();
-    }
+	// sign off
+	 @FindBy(xpath = "//input[@type='submit']")
+	public WebElement signOfLink; 
+	// constructor 
+	public FitBuddyLogin (WebDriver driver) {
+		PageFactory.initElements(driver,this); 
+	}
 }
