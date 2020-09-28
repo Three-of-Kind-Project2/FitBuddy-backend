4426297a0d07b4a996f6cbd89e9119a9cf6448ae
pranavshp <udayakumarp2@vcu.edu>
2020-09-27 23:54:19 -0400

    Added more tests

--- FitBuddy/src/test/java/com/revature/models/FoodDTOTest.java
+++ FitBuddy/src/test/java/com/revature/models/FoodDTOTest.java
@@ -7,36 +7,76 @@ public class FoodDTOTest {
 
 	@Test 
 	public void testCalories() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNotNull(test.getCalories()); 
+	}
+	
+	@Test
+	public void testSetCalories() {
+		FoodDTO test = new FoodDTO();
+		test.setCalories(100);
+		assertEquals(test.getCalories(),100); 
 	}
 	
 	@Test 
 	public void testcarbs() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNotNull(test.getCarbs()); 
+	}
+	
+	@Test
+	public void testSetCarbs() {
+		FoodDTO test = new FoodDTO();
+		test.setCarbs(100);
+		assertEquals(test.getCarbs(),100); 
 	}
 	
 	@Test 
 	public void testfat() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNotNull(test.getFat()); 
+	}
+	
+	@Test
+	public void testSetFat() {
+		FoodDTO test = new FoodDTO();
+		test.setFat(100);
+		assertEquals(test.getFat(),100); 
 	}
 	
 	@Test 
 	public void testUrl() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNull(test.getUrl()); 
+	}
+	@Test
+	public void testSetUrl() {
+		FoodDTO test = new FoodDTO();
+		test.setUrl("www.test.com");
+		assertEquals(test.getUrl(),"www.test.com"); 
 	}
 	@Test 
 	public void testProtein() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNotNull(test.getProtein()); 
+	}
+	@Test
+	public void testSetProtein() {
+		FoodDTO test = new FoodDTO();
+		test.setProtein(100);
+		assertEquals(test.getProtein(),100); 
 	}
 	@Test 
 	public void testName() {
-		Food test = new Food();
+		FoodDTO test = new FoodDTO();
 		assertNull(test.getName()); 
+	}
+	
+	@Test
+	public void testSetName() {
+		FoodDTO test = new FoodDTO();
+		test.setName("Pranav Udayakumar");
+		assertEquals(test.getName(),"Pranav Udayakumar"); 
 	}
 	
 }
